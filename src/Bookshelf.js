260534ff473dddc03db635adcc6c6776b4be00a4
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-07-09 12:21:42 +0200

    feat: Add bookshelf and book components

---
+++ src/Bookshelf.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Book from './Book.js'
+
+const Bookshelf = ({books, shelf}) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{
+      shelf
+        .replace(/([A-Z])/g, ' $1')
+        .replace(/^./, str => str.toUpperCase() )
+        // inspired by https://stackoverflow.com/questions/4149276/javascript-camelcase-to-regular-form
+    }</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {books
+          .filter(book => book.shelf === shelf)
+          .map(book => (<li key={book.id}><Book book={book}/></li>))
+        }
+      </ol>
+    </div>
+  </div>
+
+);
+
+export default Bookshelf;
