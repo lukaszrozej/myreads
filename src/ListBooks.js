3c10a662feaf3282ba8241f455e24cdcce52364c
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-07-10 09:52:08 +0200

    feat: Add search component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -8,11 +8,9 @@ const ListBooks = ({books, update}) => (
       <h1>MyReads</h1>
     </div>
     <div className="list-books-content">
-      <div>
-        <Bookshelf books={books} shelf="currentlyReading" update={update} />
-        <Bookshelf books={books} shelf="wantToRead" update={update} />
-        <Bookshelf books={books} shelf="read" update={update} />
-      </div>
+      <Bookshelf books={books} shelf="currentlyReading" update={update} />
+      <Bookshelf books={books} shelf="wantToRead" update={update} />
+      <Bookshelf books={books} shelf="read" update={update} />
     </div>
     <div className="open-search">
       <Link to="/search">Add a book</Link>
